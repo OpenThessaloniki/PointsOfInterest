1777ce295c7716738332d72020b4f59a0f175c6d
Apostolos Kritikos <akritiko@gmail.com>
2013-12-19 01:34:31 +0200

    fix leaflet

--- index.php
+++ index.php
@@ -38,28 +38,7 @@
 </div>
 
 <script type="text/javascript">
-  var lat;
-  var lng;
-
-  navigator.geolocation.getCurrentPosition( function ( pos ) {
-    lat = pos.coords.latitude;
-    lng = pos.coords.longitude;
-    $("#lat").text (lat);
-    $("#lng").text (lng);
-  } );
-
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  var marker = L.marker([51.5, -0.09]).addTo(map);
-  var circle = L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-  }).addTo(map);
-  var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-  ]).addTo(map);
+ 
 </script>
 </body>
 </html>
