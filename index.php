6691710b06236d52b8d57a5af7fe997cf409c5e0
Apostolos Kritikos <akritiko@gmail.com>
2013-12-19 01:43:25 +0200

    fix leaflet

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
- <!DOCTYPE html> 
-<html> 
-<head> 
+ <!DOCTYPE html>
+<html>
+<head>
   <meta name="viewport" content="user-scalable=no,width=device-width" />
   <link rel="stylesheet" href="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
   <script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
@@ -8,21 +8,8 @@
 
   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
   <script type="text/javascript" src="js/jquery.ui.map.js"></script>
-
-  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.css" />
- <!--[if lte IE 8]>
-     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.ie.css" />
- <![endif]-->
-
-  <script src="http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.js"></script>
-
-  <style type="text/css">
-    #map { 
-      height: 180px; 
-    }
-  </style>
-</head> 
-<body> 
+</head>
+<body>
 <div data-role="page" id="home">
   <div data-role="header">
     <h1 style="">OpenThessaloniki</h1>
@@ -31,14 +18,73 @@
     <span>Latitude:</span> <span id="lat"></span> <br>
     <span>Longitude:</span> <span id="lng"></span> <br>
   </div>
-  <!-- <div data-role="content" id="map_canvas" style="height: 300px;">
+  <div data-role="content" id="map_canvas" style="height: 300px;">
     
-  </div> -->
-  <div id="map"></div>
+  </div>
 </div>
 
 <script type="text/javascript">
- 
+  var lat;
+  var lng;
+
+  navigator.geolocation.getCurrentPosition( function ( pos ) {
+    lat = pos.coords.latitude;
+    lng = pos.coords.longitude;
+    $("#lat").text (lat);
+    $("#lng").text (lng);
+  } );
+
+  $('#map_canvas').gmap().bind('init', function(ev, map) {
+      $('#map_canvas').gmap('addMarker', {'position': lat+','+lng, 'bounds': true}).click(function() {
+      $('#map_canvas').gmap('openInfoWindow', {'content': 'You are here!'}, this);
+    });
+  });
 </script>
+
+</body>
+</html>
+ <!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="user-scalable=no,width=device-width" />
+  <link rel="stylesheet" href="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+  <script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+  <script src="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+  <script type="text/javascript" src="js/jquery.ui.map.js"></script>
+</head>
+<body>
+<div data-role="page" id="home">
+  <div data-role="header">
+    <h1 style="">OpenThessaloniki</h1>
+  </div>
+  <div data-role="content">
+    <span>Latitude:</span> <span id="lat"></span> <br>
+    <span>Longitude:</span> <span id="lng"></span> <br>
+  </div>
+  <div data-role="content" id="map_canvas" style="height: 300px;">
+    
+  </div>
+</div>
+
+<script type="text/javascript">
+  var lat;
+  var lng;
+
+  navigator.geolocation.getCurrentPosition( function ( pos ) {
+    lat = pos.coords.latitude;
+    lng = pos.coords.longitude;
+    $("#lat").text (lat);
+    $("#lng").text (lng);
+  } );
+
+  $('#map_canvas').gmap().bind('init', function(ev, map) {
+      $('#map_canvas').gmap('addMarker', {'position': lat+','+lng, 'bounds': true}).click(function() {
+      $('#map_canvas').gmap('openInfoWindow', {'content': 'You are here!'}, this);
+    });
+  });
+</script>
+
 </body>
 </html>
